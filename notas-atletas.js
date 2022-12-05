1d060eb94b2ded20d4fc29e8fb15a2f50ad8addd
Cezar Ricardo Krutsch <68482610+RicardoI9TI@users.noreply.github.com>
2022-12-05 16:00:53 -0300

    Create notas-atletas.js
    Criação do projeto nota-atletas

---
+++ notas-atletas.js
@@ -0,0 +1,34 @@
+let atletas = [
+  {
+    nome: "Cesar Abascal",
+    notas: [10, 9.34, 8.42, 10, 7.88]
+  },
+  {
+    nome: "Fernando Puntel",
+    notas:  [8, 10, 10, 7, 9.33]
+  },
+  {
+    nome: "Daiane Jelinsky",
+    notas: [7, 10, 9.5, 9.5, 8]
+  },
+  {
+    nome: "Bruno Castro",
+    notas: [10, 10, 10, 9, 9.5]
+  }
+ ];
+ 
+ for (let i = 0; i < atletas.length; i++){
+   let notas = atletas[i].notas;
+   notas = notas.sort((a, b) => a - b);
+   notas = notas.slice(1,notas.length-1);
+ 
+   let somaNota = notas.reduce(function(total, nota){
+     return total + nota;  
+   },0);
+ 
+   let mediaNota = somaNota / notas.length;
+ 
+   console.log(`Atleta: ${atletas[i].nome}`);
+   console.log(`Notas Obtidas: ${atletas[i].notas}`);
+   console.log(`Média Válida: ${mediaNota}`); 
+ }
